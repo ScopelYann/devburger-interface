d9d45a76ecd25b5635988a02be3c32d4950aab5a
Scopel_Yan <160073076+ScopelYann@users.noreply.github.com>
2025-01-24 21:33:51 -0300

    newDockerfile

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3200',
+  baseURL: 'http://127.0.0.1:3200',
 });
 
 api.interceptors.request.use((config) => {
